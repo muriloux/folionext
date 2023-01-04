315878ba73d077ff29e7dd6be754a19731d41825
muriloux <murilomelo.code@gmail.com>
2023-01-04 02:31:49 -0300

    404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,21 @@
 export default function NotFound() {
   return (
     <>
-      <h1>Page not found</h1>
-      <h2>404</h2>
+      <div className="container flex flex-col justify-center items-center pt-5">
+        <h1 className="font-bold text-slate-900">Not found 404</h1>
+        <h2 className="pb-10 text-slate-500">What happened?</h2>
+        <video autoPlay={true} loop muted width={500} height={500}>
+          <source src="/where.mp4" />
+        </video>
+      </div>
     </>
   );
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      is404: true,
+    },
+  };
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,15 @@ import { Navbar } from "../components/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className="px-6 sm:px-36 md:px-52 py-32 pt-3">
-      <Navbar />
+    <main
+      style={
+        pageProps.is404
+          ? { backgroundColor: "white" }
+          : { backgroundColor: "var(--background)" }
+      }
+      className="px-6 sm:px-36 md:px-52 py-32 pt-3"
+    >
+      {pageProps.is404 ? <></> : <Navbar />}
       <Component {...pageProps} />
     </main>
   );
