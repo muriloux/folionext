83b35f4e27b9ad861d146a9af3e458ee2889816d
muriloux <murilomelo.code@gmail.com>
2023-01-08 04:23:01 -0300

    better menu UI on small and medium devices

--- components/Navbar/NavItems/index.tsx
+++ components/Navbar/NavItems/index.tsx
@@ -19,7 +19,7 @@ export const NavItems: FunctionComponent<IProps> = ({ open, setOpen }) => {
       id="NavItems"
       className={`${
         open ? "visible opacity-100" : "invisible opacity-0"
-      } flex flex-col  fixed z-10 bg-[var(--background)] dark:bg-zinc-900 md:inline-block md:visible md:opacity-100 md:relative md:flex-row transition-all`}
+      } flex flex-col z-10 bg-[var(--background)] left-0 min-w-full absolute dark:bg-zinc-900 md:inline-block md:visible md:opacity-100 md:relative md:flex-row transition-all`}
     >
       <NavItem name="Home" href="/" />
       <NavItem name="Skills" href="/#skills" />
