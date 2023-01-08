71c26c6735960d653b14b6efd1b205ad0513e4be
muriloux <murilomelo.code@gmail.com>
2023-01-08 07:24:40 -0300

    _app refactoring

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,12 +5,9 @@ import { Navbar } from "../components/Navbar";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main
-      style={
-        pageProps.is404
-          ? { backgroundColor: "white" }
-          : { backgroundColor: "var(--background)" }
-      }
-      className={"px-6 sm:px-12 md:px-52 py-32 pt-3"}
+      className={`${
+        pageProps.is404 ? "bg-white" : "bg-[var(--background)]"
+      } px-6 sm:px-12 md:px-52 py-32 pt-3`}
     >
       {pageProps.is404 ? <></> : <Navbar />}
       <Component {...pageProps} />
